9477f42d300d77362bad51aa20dd8c300773b6f5
anutka-mak <annmakiichuk@gmail.com>
2023-08-17 01:09:28 +0300

    all update

--- script.js
+++ script.js
@@ -3,54 +3,80 @@ const cards = document.querySelectorAll('.cards');
 const prevButton = document.getElementById('button-prev');
 const nextButton = document.getElementById('button-next');
 
-const catalogs = ['catalog-about-houses', 'catalog-about-villas', 'catalog-about-apartaments'];
+let catalogs = [
+    { index: 0, name: 'catalog-about-houses' },
+    { index: 1, name: 'catalog-about-villas' },
+    { index: 2, name: 'catalog-about-apartments' },
+];
 let currentCatalogIndex = 0;
 
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-    buttons.forEach(btn => btn.classList.remove('active'));
-    button.classList.add('active');
-    cards.forEach(card => card.classList.remove('active'));
-    document.getElementById(catalogs[index]).classList.add('active');
-    currentCatalogIndex = index;
-    updateButtons();
-  });
+        removeClassListButton(button);
+        addClassListButton(button);
+        cards.forEach(card => card.classList.remove('active'));
+        document.getElementById(catalogs[index].name).classList.add('active');
+        currentCatalogIndex = index;
+        updateButtons();
+    });
 });
 
 prevButton.addEventListener('click', () => {
-  if (currentCatalogIndex > 0) {
-    currentCatalogIndex--;
-  }
-  updateCatalog(currentCatalogIndex);
-  updateButtons();
+    if (currentCatalogIndex > 0) {
+        currentCatalogIndex--;
+    }
+    updateCatalog(currentCatalogIndex);
+    updateButtons();
 });
 
 nextButton.addEventListener('click', () => {
-  if (currentCatalogIndex < catalogs.length - 1) {
-    currentCatalogIndex++;
-  }
-  updateCatalog(currentCatalogIndex);
-  updateButtons();
+    if (currentCatalogIndex < catalogs.length - 1) {
+        currentCatalogIndex++;
+    }
+    updateCatalog(currentCatalogIndex);
+    updateButtons();
 });
 
 function updateButtons() {
     if (currentCatalogIndex === 0) {
-      nextButton.classList.add('active') 
-      prevButton.classList.remove('active')
-    } else if (currentCatalogIndex === 1) {
-        prevButton.classList.add('active')
-        nextButton.classList.add('active')
-    } else if (currentCatalogIndex === 2) {
-      prevButton.classList.add('active')
-      nextButton.classList.remove('active')
+        addClassListNextButton();
+        removeClassListPrevButton();
+    } else if (currentCatalogIndex === catalogs.length - 1) {
+        addClassListPrevButton()
+        removeClassListNextButton();
+    } else {
+        addClassListPrevButton();
+        addClassListNextButton();
     }
 }
 
 function updateCatalog(index) {
-  buttons.forEach(btn => btn.classList.remove('active'));
-  buttons[index].classList.add('active');
-  cards.forEach(card => card.classList.remove('active'));
-  cards[index].classList.add('active');
+    buttons.forEach(button => button.classList.remove('active'));
+    cards.forEach(card => card.classList.remove('active'));
+    buttons[index].classList.add('active');
+    cards[index].classList.add('active');
 }
 
+function removeClassListButton(button) {
+    buttons.forEach(button => button.classList.remove('active'));
+}
 
+function addClassListButton(button) {
+    button.classList.add('active');
+}
+
+function addClassListNextButton() {
+    nextButton.classList.add('active');
+}
+
+function removeClassListNextButton() {
+    nextButton.classList.remove('active');
+}
+
+function addClassListPrevButton() {
+    prevButton.classList.add('active');
+}
+
+function removeClassListPrevButton() {
+    prevButton.classList.remove('active');
+}
